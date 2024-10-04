570eea289e2330e735dc21c7e421f11e85a80254
Marcos Candanedo <marcos.candanedo1@utp.ac.pa>
2024-10-04 22:10:23 +0000

    add: login

---
+++ frontend/src/app/login/login.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { LoginComponent } from './login.component';
+import { LoginRoutingModule } from './login-routing.module';
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    IonicModule,
+    LoginRoutingModule
+  ]
+})
+export class LoginModule { }
+
